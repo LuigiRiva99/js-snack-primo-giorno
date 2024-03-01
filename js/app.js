edd1b3ba4d1f88f5e58b891b247841e40f9a74cb
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-01 14:39:35 +0100

    Esercizi js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// Esercizio 1
+const number = 100; //number
+console.log(number);
+
+
+// Esercizio 2 
+let PI = 3.14; //number       --> per poter cambiare il valore a PI devo usare "let" 
+console.log(PI);
+
+// Esercizio 2B 
+PI = 3.1416; //number
+console.log(PI);
+
+// Esercizio 2C
+let radius = 8; //number
+let circle = radius * 2 * PI; //number
+console.log(circle);
+
+// Esercizio 3
+let name= "Carlo"; //string
+name = "Marco"; //string
+
+console.log(name)
+
+
